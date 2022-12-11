15acb0786410fa00bcb877d3712ecd8694e4be8c
Maximilian <scientificresearch309@gmail.com>
2022-12-11 16:42:58 +0100

    Initialization was completed. front-backend work.

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { PromiseProvider } = require("mongoose");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send(`Hi, I am from SERVER site on PORT ${PORT}`);
+});
