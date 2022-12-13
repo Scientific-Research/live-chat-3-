f261341fdf1060ae9331e3a56620692e14dc331a
Maximilian <scientificresearch309@gmail.com>
2022-12-13 13:56:22 +0100

    changes

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      <h1>This is the Login Page-13.12.2022.</h1>
+      <h1>This is the Login Page-13.12.2022sdasdasd.</h1>
     </div>
   );
 };
