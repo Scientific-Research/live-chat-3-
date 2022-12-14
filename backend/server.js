c285cda5c7ca987d3ba982fc601b93c39be55ef8
Maximilian <scientificresearch309@gmail.com>
2022-12-14 20:17:29 +0100

    Registration in authController is working!

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const databaseConnect = require("./config/database");
+const authRouter = require("./routes/authRoute");
 
+dotenv.config({
+  path: "backend/config/config.env",
+});
+
+app.use("/api/messenger", authRouter);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
+
+databaseConnect();
 
 app.get("/", (req, res) => {
   res.send(`Hi, I am from SERVER site on PORT ${PORT}`);
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,6 +1,58 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { userRegister } from "../store/actions/authAction";
+
 const Register = () => {
+  const dispatch = useDispatch();
+
+  const [state, setState] = useState({
+    userName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    image: "",
+  });
+
+  const [loadImage, setLoadImage] = useState("");
+  const inputHandle = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const fileHandle = (e) => {
+    if (e.target.files.length !== 0) {
+      setState({
+        ...state,
+        [e.target.name]: e.target.files[0],
+      });
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      setLoadImage(reader.result);
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
+
+  const register = (e) => {
+    const {userName, email, password, confirmPassword, image} = state;
+
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("userName", userName);
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("confirmPassword", confirmPassword);
+    formData.append("image", image);
+
+    dispatch(userRegister(formData));
+    console.log(state);
+  };
+
   return (
     <div className="register">
       <div className="card">
@@ -8,11 +60,14 @@ const Register = () => {
           <h3>Register</h3>
         </div>
         <div className="card-body">
-          <form action="">
+          <form onSubmit={register}>
             <div className="form-group">
               <label htmlFor="username">User Name</label>
               <input
                 type="text"
+                onChange={inputHandle}
+                value={state.userName}
+                name="userName"
                 className="form-control"
                 placeholder="User Name"
                 id="username"
@@ -23,6 +78,9 @@ const Register = () => {
               <label htmlFor="email">Email</label>
               <input
                 type="email"
+                onChange={inputHandle}
+                value={state.email}
+                name="email"
                 className="form-control"
                 placeholder="Email"
                 id="email"
@@ -33,6 +91,9 @@ const Register = () => {
               <label htmlFor="password">Password</label>
               <input
                 type="password"
+                onChange={inputHandle}
+                value={state.password}
+                name="password"
                 className="form-control"
                 placeholder="Password"
                 id="password"
@@ -43,6 +104,9 @@ const Register = () => {
               <label htmlFor="confirmPassword">Confirm Password</label>
               <input
                 type="password"
+                onChange={inputHandle}
+                value={state.confirmPassword}
+                name="confirmPassword"
                 className="form-control"
                 placeholder="Confirm Password"
                 id="confirmPassword"
@@ -51,11 +115,20 @@ const Register = () => {
 
             <div className="form-group">
               <div className="file-image">
-                <div className="image"></div>
+                <div className="image">
+                  {loadImage ? <img src={loadImage} alt="" /> : ""}
+                </div>
 
                 <div className="file">
                   <label htmlFor="image">Select Image</label>
-                  <input type="file" className="form-control" id="image" />
+                  <input
+                    type="file"
+                    onChange={fileHandle}
+                    // value={state.userName}
+                    name="image"
+                    className="form-control"
+                    id="image"
+                  />
                 </div>
               </div>
             </div>
